797f7ff5ad357b5fb59ef1c795a0a1dcd35cdc8c
Yachao <yachao@gwmail.gwu.edu>
2017-04-17 16:29:49 -0400

    restore original index.js

--- index.js
+++ index.js
@@ -1,166 +1,136 @@
-// 'use strict';
-// var GitHubApi = require("github");
-// // var GitHubApi = require('github');
-// var matter = require('gray-matter');
-// var mkdirp = require('mkdirp');
-// var Bluebird = require('bluebird');
+'use strict';
+var GitHubApi = require('node-github');
+var Octokat = require('octokat');
+var matter = require('gray-matter');
+var mkdirp = require('mkdirp');
+var Bluebird = require('bluebird');
 
+//We authenticate to prevent API rate limits
+var octo = new Octokat({
+    token: process.env.NODE_GITHUB_TOKEN
+});
 
-var GitHubApi = require("github");
-
+/*
+ * Im mixing both github libraries
+ * because node-github does not implement
+ * getContent, and I already had the fork
+ * part working, I did not see how to do it
+ * using Octokat...
+ * I will probably refactor later.
+ */
 var github = new GitHubApi({
-    // optional
+    version: '3.0.0',
     debug: true,
-    protocol: "https",
-    host: "github.my-GHE-enabled-company.com", // should be api.github.com for GitHub
-    pathPrefix: "/api/v3", // for some GHEs; none for GitHub
+    protocol: 'https',
+    host: 'api.github.com',
+    pathPrefix: '/api/v3',
+    timeout: 5000,
     headers: {
-        "user-agent": "My-Cool-GitHub-App" // GitHub is happy with a unique user agent
-    },
-    Promise: require('bluebird'),
-    followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
-    timeout: 5000
+        'user-agent': 'My-Cool-GitHub-App'
+    }
 });
 
-// TODO: optional authentication here depending on desired endpoints. See below in README.
-
-github.users.getFollowingForUser({
-    // optional
-    // headers: {
-    //     "cookie": "blahblah"
-    // },
-    username: "defunkt"
-}, function(err, res) {
-    console.log(JSON.stringify(res));
+github.authenticate({
+    type: 'oauth',
+    token: process.env.NODE_GITHUB_TOKEN
 });
 
+var repository = 'studyGroup';
+var username = 'mozillascience';
 
-// var github = new GitHubApi({
-//     // optional
-//     debug: true,
-//     protocol: "https",
-//     host: "github.my-GHE-enabled-company.com", // should be api.github.com for GitHub
-//     pathPrefix: "/api/v3", // for some GHEs; none for GitHub
-//     headers: {
-//         "user-agent": "My-Cool-GitHub-App" // GitHub is happy with a unique user agent
-//     },
-//     Promise: require('bluebird'),
-//     followRedirects: false, // default: true; there's currently an issue with non-get redirects, so allow ability to disable follow-redirects
-//     timeout: 5000
-// });
+var offset = 0;
+var pageSize = 33;
 
-// var github = new GitHubApi({
-//     debug: true,
-//     protocol: 'https',
-//     host: 'api.github.com',
-//     pathPrefix: '/api/v3',
-//     timeout: 5000,
-//     headers: {
-//         'user-agent': 'My-Cool-GitHub-App'
-//     }
-// });
+getForkCount(username, repository).then(function(total){
 
-// github.authenticate({
-//     type: 'oauth',
-//     token: '81e18c18fabdceba33aede480c46fe5e650f04c0'
-// });
+    var promise, promises = [];
+    for(offset=0; offset < Math.ceil(total/pageSize); offset++){
+        promise = processForkBatch(username, repository, offset, pageSize);
+        promises.push(promise);
+    }
 
-// var repository = 'studyGroup';
-// var username = 'mozillascience';
+    Bluebird.all(promises).then(function(batches){
+        batches.map(function(batch){
+            processContent(batch);
+        });
+    }).catch(function(err){
+        console.error('Batch download error');
+        console.error('ERROR: %s.\n%s', err.message, err.stack);
+    });
+});
 
-// var offset = 0;
-// var pageSize = 33;
+function getForkCount(user, repo){
+    return new Bluebird(function(resolve, reject){
+        github.repos.get({
+            user: user,
+            repo: repo,
+        }, function(err, res){
+            console.log('Total forks', res.forks);
+            if(err) return reject(err);
+            resolve(res.forks);
+        });
+    });
+}
 
-// getForkCount(username, repository).then(function(total){
+function processForkBatch(user, repo, offset, size){
+    if(size > 100) size = 100;
 
-//     var promise, promises = [];
-//     for(offset=0; offset < Math.ceil(total/pageSize); offset++){
-//         promise = processForkBatch(username, repository, offset, pageSize);
-//         promises.push(promise);
-//     }
+    return new Bluebird(function(resolve, reject){
+        //First we need to know how many forks we have,
+        //since we will have to paginage.
+        github.repos.getForks({
+            user: user,
+            repo: repo,
+            page: offset,
+            per_page: size
+        }, function(err, res) {
+            if(err) return reject(err);
 
-//     Bluebird.all(promises).then(function(batches){
-//         batches.map(function(batch){
-//             processContent(batch);
-//         });
-//     }).catch(function(err){
-//         console.error('Batch download error');
-//         console.error('ERROR: %s.\n%s', err.message, err.stack);
-//     });
-// });
+            console.log('Batch size', res.length);
+            var forks = [];
+            res.map(function(fork){
+                forks.push({
+                    user: fork.owner.login,
+                    repo: fork.name,
+                    path: '_posts',
+                    ref: 'gh-pages',
+                    full_name: fork.full_name
+                });
+            });
+            resolve(forks);
+        });
+    });
+}
 
-// function getForkCount(user, repo){
-//     return new Bluebird(function(resolve, reject){
-//         github.repos.get({
-//             user: user,
-//             repo: repo,
-//         }, function(err, res){
-//             console.log('Total forks', res.forks);
-//             if(err) return reject(err);
-//             resolve(res.forks);
-//         });
-//     });
-// }
+function processContent(forks){
 
-// function processForkBatch(user, repo, offset, size){
-//     if(size > 100) size = 100;
+    console.log("FORTS", JSON.stringify(forks, null, 4));
+    forks.map(function(fork){
+        var repo = octo.repos(fork.user, fork.repo);
+        var filepath = require('path').resolve('./data/'+ fork.user);
+        mkdirp.sync(filepath);
+        repo.contents('_posts').fetch().then(function(contents) {        // `.fetch` is used for getting JSON
 
-//     return new Bluebird(function(resolve, reject){
-//         //First we need to know how many forks we have,
-//         //since we will have to paginage.
-//         github.repos.getForks({
-//             user: user,
-//             repo: repo,
-//             page: offset,
-//             per_page: size
-//         }, function(err, res) {
-//             if(err) return reject(err);
+            var promises = [];
+            contents.map(function(file){
+                console.log(file.path);
+                //use bluebird spread
+                promises.push(repo.contents(file.path).read());
+            });
 
-//             console.log('Batch size', res.length);
-//             var forks = [];
-//             res.map(function(fork){
-//                 forks.push({
-//                     user: fork.owner.login,
-//                     repo: fork.name,
-//                     path: '_posts',
-//                     ref: 'gh-pages',
-//                     full_name: fork.full_name
-//                 });
-//             });
-//             resolve(forks);
-//         });
-//     });
-// }
-
-// function processContent(forks){
-
-//     console.log("FORTS", JSON.stringify(forks, null, 4));
-//     forks.map(function(fork){
-//         var repo = octo.repos(fork.user, fork.repo);
-//         var filepath = require('path').resolve('./data/'+ fork.user);
-//         mkdirp.sync(filepath);
-//         repo.contents('_posts').fetch().then(function(contents) {        // `.fetch` is used for getting JSON
-
-//             var promises = [];
-//             contents.map(function(file){
-//                 console.log(file.path);
-//                 //use bluebird spread
-//                 promises.push(repo.contents(file.path).read());
-//             });
-
-//             var file = [];
-//             Bluebird.all(promises).then(function(files){
-//                 files.map(function(contents){
-//                     file.push(matter(contents).data);
-//                 });
-//                 require('fs').writeFileSync(filepath + '/data.json', JSON.stringify(file, null, 4), 'utf-8');
-//             }).catch(function(err){
-//                 console.error('Batch create file error');
-//                 console.error('ERROR: %s.\n%s', err.message, err.stack);
-//             });
-//         }).catch(function(err){
-//             console.error('Fetch repo _posts error');
-//             console.error('ERROR: %s.\n%s', err.message, err.stack);
-//         });
-//     });
-// }
+            var file = [];
+            Bluebird.all(promises).then(function(files){
+                files.map(function(contents){
+                    file.push(matter(contents).data);
+                });
+                require('fs').writeFileSync(filepath + '/data.json', JSON.stringify(file, null, 4), 'utf-8');
+            }).catch(function(err){
+                console.error('Batch create file error');
+                console.error('ERROR: %s.\n%s', err.message, err.stack);
+            });
+        }).catch(function(err){
+            console.error('Fetch repo _posts error');
+            console.error('ERROR: %s.\n%s', err.message, err.stack);
+        });
+    });
+}
